1528c644df3365fef2fb5d1aff4e549b3f54e009
JohnMayes <JohnMayes51@gmail.com>
2022-05-09 16:48:29 -0500

    quick-start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { QuickStart } from './quick-start';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className={'w-screen h-screen flex place-content-center'}>
+      <QuickStart />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/quick-start.tsx
@@ -0,0 +1,96 @@
+// @ts-nocheck
+import { useMemo } from 'react';
+import { useTable } from 'react-table';
+
+export const QuickStart = () => {
+  const data = useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    []
+  );
+
+  const tableInstance = useTable({ columns, data });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <div className="flex flex-col place-content-center">
+      <table {...getTableProps()}>
+        <thead className={'bg-slate-600 text-xl text-gray-200'}>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {
+            // Loop over the table rows
+            rows.map((row) => {
+              // Prepare the row for display
+              prepareRow(row);
+              return (
+                // Apply the row props
+                <tr
+                  {...row.getRowProps()}
+                  className={'outline outline-1 outline-black'}
+                >
+                  {
+                    // Loop over the rows cells
+                    row.cells.map((cell) => {
+                      // Apply the cell props
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Render the cell contents
+                            cell.render('Cell')
+                          }
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
