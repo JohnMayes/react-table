e03bde10fc94eb7e266a28c28ccd3e8f1aa006af
JohnMayes <JohnMayes51@gmail.com>
2022-05-09 21:32:54 -0500

    wip test-table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useMemo } from 'react';
 import { QuickStart } from './quick-start';
+import TestTable from './test-table';
 import './App.css';
 
 function App() {
+  const columns = useMemo(
+    () => [
+      { Header: 'TITLE / DESCRIPTION', accessor: 'col1' },
+      { Header: 'TYPE', accessor: 'col2' },
+      { Header: 'IVAC', accessor: 'col3' },
+      { Header: 'VOTING SYSTEM TYPE', accessor: 'col4' },
+      { Header: 'STATE / COUNTY', accessor: 'col5' },
+      { Header: 'VOTES AFFECTED', accessor: 'col6' },
+      { Header: 'EVENT DATE', accessor: 'col7' },
+    ],
+    []
+  );
+
   return (
     <div className={'w-screen h-screen flex place-content-center'}>
-      <QuickStart />
+      <TestTable columns={columns} data={''} />
     </div>
   );
 }
