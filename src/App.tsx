eba73e999929762c0de0dcf1bb2216ea6c051bcf
JohnMayes <JohnMayes51@gmail.com>
2022-05-11 11:31:18 -0500

    init BasicTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { useMemo } from 'react';
-import { QuickStart } from './quick-start';
-import TestTable from './test-table';
+import { BasicTable } from './components/BasicTable';
+
 import './App.css';
 
 function App() {
-  const columns = useMemo(
-    () => [
-      { Header: 'TITLE / DESCRIPTION', accessor: 'col1' },
-      { Header: 'TYPE', accessor: 'col2' },
-      { Header: 'IVAC', accessor: 'col3' },
-      { Header: 'VOTING SYSTEM TYPE', accessor: 'col4' },
-      { Header: 'STATE / COUNTY', accessor: 'col5' },
-      { Header: 'VOTES AFFECTED', accessor: 'col6' },
-      { Header: 'EVENT DATE', accessor: 'col7' },
-    ],
-    []
-  );
-
   return (
-    <div className={'w-screen h-screen flex place-content-center'}>
-      <TestTable columns={columns} data={''} />
+    <div className={'w-screen flex flex-col place-items-center p-10'}>
+      <h1 className={'text-3xl font-extrabold'}>React Table</h1>
+
+      <BasicTable />
     </div>
   );
 }
---
+++ src/components/BasicTable.tsx
@@ -0,0 +1,70 @@
+// @ts-nocheck
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './COLUMNS';
+
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <div className={'w-8/12'}>
+      <table
+        {...getTableProps()}
+        className="min-w-full divide-y divide-gray-300"
+      >
+        <thead className="bg-gray-50">
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody
+          {...getTableBodyProps()}
+          className="divide-y divide-gray-200 bg-white"
+        >
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
